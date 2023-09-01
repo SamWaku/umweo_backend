5e48f3a9c141587c9b52a8ce3773e283a83456b6
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-01 19:01:11 +0200

    validation

--- src/v1.0/controllers/task-controller.js
+++ src/v1.0/controllers/task-controller.js
@@ -3,7 +3,11 @@ const taskService = require("../services/task-service");
 const TaskController = () => {
   const CreateTask = async (req, res) => {
     try {
-      const task = await taskService.createTask(req.body);
+      const data = req.body;
+      if(!data){
+        return res.status(400).json("Enter valid data!")
+      }
+      const task = await taskService.createTask(data);
       res.status(200).json(task);
     } catch (error) {
       res.status(400).json(error);
@@ -45,7 +49,7 @@ const TaskController = () => {
     CreateTask,
     GetTaskById,
     UpdateTaskById,
-    DeleteTaskById
+    DeleteTaskById,
   };
 };
 
