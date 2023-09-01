3a9f6560d821b4279526a43a5bd179081dc7e008
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-01 19:13:11 +0200

    validation

--- src/v1.0/services/task-service.js
+++ src/v1.0/services/task-service.js
@@ -1,21 +1,29 @@
 const taskRepository = require("../repositories/task-repository");
 
 const TaskService = () => {
+  // Create task
   const createTask = async (data) => {
     const task = await taskRepository.createTask(data);
     return task;
   };
+
+  // Get task by id
   const getTaskById = async (id) => {
     const task = await taskRepository.getTaskById(id);
     return task;
   };
-  const updateTask = async (id,data) => {
-    const updatedTask = await taskRepository.updateTask(id,data);
+
+  // Update task by id
+  const updateTask = async (id, data) => {
+    const updatedTask = await taskRepository.updateTask(id, data);
     return updatedTask;
   };
+
+  // Delete task by id
   const deleteTask = async (id) => {
     await taskRepository.deleteTaskById(id);
   };
+  
   return {
     createTask,
     getTaskById,
