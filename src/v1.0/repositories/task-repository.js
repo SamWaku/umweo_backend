527ebe3873eac59d8a2096bb42b6b25a7d55cf94
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-01 18:32:21 +0200

    chnages

--- src/v1.0/repositories/task-repository.js
+++ src/v1.0/repositories/task-repository.js
@@ -2,44 +2,46 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const TaskRepository=()=>{
-    const createTask=async(data)=>{
-        return prisma.tasks.create({
-            data
-        })
-    }
-    const getTasks=async()=>{
-        return prisma.tasks.findMany();
-    }
-    const getTaskById=async(id)=>{
-        return prisma.tasks.findUnique({
-            where:{
-                id
-            }
-        })
-    }
-    const updateTask=async(id,data)=>{
-        return prisma.tasks.update({
-            where:{
-                id
-            },
-            data
-        })
-    }
-    const deleteTaskById=async(id)=>{
-        return prisma.tasks.delete({
-            where:{
-                id
-            }
-        })
-    }
-    return{
-        createTask,
-        getTasks,
-        getTaskById,
-        updateTask,
-        deleteTaskById
-    }
-}
+const TaskRepository = () => {
+  const createTask = async (data) => {
+    return prisma.tasks.create({
+      data,
+    });
+  };
+  const getTasks = async () => {
+    return prisma.tasks.findMany();
+  };
+  const getTaskById = async (id) => {
+    return prisma.tasks.findUnique({
+      where: {
+        id,
+      },
+    });
+  };
+  const updateTask = async (id, data) => {
+    return prisma.tasks.update({
+      where: {
+        id,
+      },
+      data: {
+        data,
+      },
+    });
+  };
+  const deleteTaskById = async (id) => {
+    return prisma.tasks.delete({
+      where: {
+        id,
+      },
+    });
+  };
+  return {
+    createTask,
+    getTasks,
+    getTaskById,
+    updateTask,
+    deleteTaskById,
+  };
+};
 
-module.exports=TaskRepository();
+module.exports = TaskRepository();
