7b72cd204c51b98d458bff70133eaddd46a985f5
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-01 19:09:56 +0200

    validation

--- src/v1.0/routers/task-router.js
+++ src/v1.0/routers/task-router.js
@@ -2,9 +2,16 @@ const taskController = require("../controllers/task-controller");
 
 const router = require("express").Router();
 
+// create task
 router.post("/", taskController.CreateTask);
+
+// get task by id
 router.get("/:id", taskController.GetTaskById);
+
+// update task by id
 router.put("/:id", taskController.UpdateTaskById);
+
+// delete task by id
 router.delete("/:id", taskController.DeleteTaskById);
 
 module.exports = router;
