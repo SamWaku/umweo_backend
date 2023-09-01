f57267fc6f7f02772bf8896e67b490b6b9d2ce36
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-01 16:23:30 +0200

    changes

--- src/v1.0/models/index.js
+++ src/v1.0/models/index.js
@@ -1,1 +1,27 @@
-console.log(first)
+const TaskRepository = require("../repositories/task-repository");
+
+const TaskService=()=>{
+    const createTask=async(data)=>{
+        const task=await TaskRepository.createTask(data);
+        return task;
+    }
+    const getTaskById=async(id)=>{
+        const task=await TaskRepository.getTaskById(id);
+        return task;
+    }
+    const updateTask=async(data)=>{
+        const updatedTask=await TaskRepository.updateTask(data);
+        return updatedTask;
+    }
+    const deleteTask=async(id)=>{
+        await TaskRepository.deleteTaskById(id);
+    }
+    return{
+        createTask,
+        getTaskById,
+        updateTask,
+        deleteTask
+    }
+}
+
+module.exports=TaskService();
