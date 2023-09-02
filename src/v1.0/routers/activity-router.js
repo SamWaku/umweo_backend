5fde877cfc2a52f68da3fb8dff0fb9bcafd76aaa
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-02 20:56:26 +0200

    schedule and activity

--- src/v1.0/routers/activity-router.js
+++ src/v1.0/routers/activity-router.js
@@ -4,5 +4,6 @@ const ActivityRouter = require("express").Router();
 ActivityRouter.post("/", activityController.CreateActivity);
 ActivityRouter.get("/:id", activityController.GetActivityById);
 ActivityRouter.put("/:id", activityController.UpdateActivityById);
+ActivityRouter.delete("/:id", activityController.DeleteActivityByID);
 
 module.exports = ActivityRouter;
