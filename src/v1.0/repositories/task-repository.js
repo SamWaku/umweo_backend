cc49b39917cc8db46246e32b0d358168048d49b4
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-02 09:26:27 +0200

    schema changes

--- src/v1.0/repositories/task-repository.js
+++ src/v1.0/repositories/task-repository.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+const { prisma } = require("../config/db");
 
 const TaskRepository = () => {
   // Create task
