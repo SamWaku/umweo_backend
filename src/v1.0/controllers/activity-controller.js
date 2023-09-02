9e831e46d60495bd4c2e85dc6296bd30f270ea65
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-02 20:29:50 +0200

    schedule and activity

--- src/v1.0/controllers/activity-controller.js
+++ src/v1.0/controllers/activity-controller.js
@@ -4,6 +4,9 @@ const ActivityController = () => {
   const createActivity = async (req, res) => {
     try {
       const data = req.body;
+      if(!data){
+        return res.status(400).json("Enter valid data");
+      }
       const activity =await activityService.createActivity(data);
       res.status(200).json(activity);
     } catch (error) {
