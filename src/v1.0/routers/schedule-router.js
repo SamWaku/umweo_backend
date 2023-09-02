fe46e3b7e14145ae649e9961a96760c3d2ee55a7
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-02 21:12:32 +0200

    comments

--- src/v1.0/routers/schedule-router.js
+++ src/v1.0/routers/schedule-router.js
@@ -1,16 +1,16 @@
 const scheduleController = require("../controllers/schedule-controller");
 const ScheduleRouter = require("express").Router();
 
+// post schedule i.e /schedule
+ScheduleRouter.post("/", scheduleController.CreateSchedule);
+
 // get schedule by ID i.e /schedule/:id
 ScheduleRouter.get("/:id", scheduleController.GetScheduleByID);
-
-// delete schedule by ID i.e /schedule/:id
-ScheduleRouter.delete("/:id", scheduleController.DeleteScheduleByID);
 
 // update schedule by ID i.e /schedule/:id
 ScheduleRouter.put("/:id", scheduleController.UpdateScheduleByID);
 
-// post schedule i.e /schedule
-ScheduleRouter.post("/", scheduleController.CreateSchedule);
+// delete schedule by ID i.e /schedule/:id
+ScheduleRouter.delete("/:id", scheduleController.DeleteScheduleByID);
 
 module.exports = ScheduleRouter;
