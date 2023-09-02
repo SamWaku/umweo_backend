77a478fc634871952b4aed60cd8d8ebd6de7d924
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-02 20:53:13 +0200

    schedule and activity

--- src/v1.0/routers/activity-router.js
+++ src/v1.0/routers/activity-router.js
@@ -3,5 +3,6 @@ const ActivityRouter = require("express").Router();
 
 ActivityRouter.post('/',activityController.CreateActivity);
 ActivityRouter.get('/:id',activityController.GetActivityById);
+ActivityRouter.put('/:id',activityController.UpdateActivityById);
 
 module.exports = ActivityRouter;
