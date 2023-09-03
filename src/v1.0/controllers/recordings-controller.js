f85fd6c305a5314fff769c1c85492c71ed99e2f1
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 10:39:42 +0200

    recordings

--- src/v1.0/controllers/recordings-controller.js
+++ src/v1.0/controllers/recordings-controller.js
@@ -1,68 +1,68 @@
 const recordingsService = require("../services/recordings-service");
 
-const RecordingsController=()=>{
-    const createRecording=async(req,res)=>{
-        try {
-            const data=req.body;
-            if(!data){
-                return res.status(400).json("Enter valid data!");
-            }
-            const recording=await recordingsService.CreateRecording(data);
-            res.status(200).json(recording);
-        } catch (error) {
-            res.status(400).json(error);
-            console.log(error);
-        }
+const RecordingsController = () => {
+  const createRecording = async (req, res) => {
+    try {
+      const data = req.body;
+      if (!data) {
+        return res.status(400).json("Enter valid data!");
+      }
+      const recording = await recordingsService.CreateRecording(data);
+      res.status(200).json(recording);
+    } catch (error) {
+      res.status(400).json(error);
+      console.log(error);
     }
-    const getRecordingById=async(req,res)=>{
-        try {
-            const {id}=req.params;
-            if(!id){
-                return res.status(400).json("ID doesn't exist!");
-            }
-            const recording=await recordingsService.GetRecordingByID(id);
-            res.status(200).json(recording);
-        } catch (error) {
-            res.status(400).json(error);
-            console.log(error);
-        }
+  };
+  const getRecordingById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json("ID doesn't exist!");
+      }
+      const recording = await recordingsService.GetRecordingByID(id);
+      res.status(200).json(recording);
+    } catch (error) {
+      res.status(400).json(error);
+      console.log(error);
     }
-    const updateRecordingById=async(req,res)=>{
-        try {
-            const {id}=req.params;
-            if(!id){
-                return res.status(400).json("ID doesn't exist!");
-            }
-            const data=req.body;
-            if(!data){
-                return res.status(400).json("Enter valid data!");
-            }
-            const recording=await recordingsService.UpdateRecordingByID(id,data);
-            res.status(200).json(recording);
-        } catch (error) {
-            res.status(400).json(error);
-            console.log(error);
-        }
+  };
+  const updateRecordingById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json("ID doesn't exist!");
+      }
+      const data = req.body;
+      if (!data) {
+        return res.status(400).json("Enter valid data!");
+      }
+      const recording = await recordingsService.UpdateRecordingByID(id, data);
+      res.status(200).json(recording);
+    } catch (error) {
+      res.status(400).json(error);
+      console.log(error);
     }
-    const deleteRecordingById=async(req,res)=>{
-        try {
-            const {id}=req.params;
-            if(!id){
-                return res.status(400).json("ID doesn't exist!");
-            }
-            await recordingsService.DeleteRecordingByID(id);
-            res.status(200).json("Recording Deleted");
-        } catch (error) {
-            res.status(400).json(error);
-            console.log(error);
-        }
+  };
+  const deleteRecordingById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json("ID doesn't exist!");
+      }
+      await recordingsService.DeleteRecordingByID(id);
+      res.status(200).json("Recording Deleted");
+    } catch (error) {
+      res.status(400).json(error);
+      console.log(error);
     }
-    return{
-        createRecording,
-        getRecordingById,
-        updateRecordingById,
-        deleteRecordingById
-    }
-}
+  };
+  return {
+    createRecording,
+    getRecordingById,
+    updateRecordingById,
+    deleteRecordingById,
+  };
+};
 
-module.exports=RecordingsController();
+module.exports = RecordingsController();
