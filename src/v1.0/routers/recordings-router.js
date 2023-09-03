80323361632ab2879c1de566f50a28cf242675fd
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 10:44:38 +0200

    recordings

---
+++ src/v1.0/routers/recordings-router.js
@@ -0,0 +1,9 @@
+const recordingsController = require("../controllers/recordings-controller");
+
+const RecordingsRouter=require('express').Router();
+
+RecordingsRouter.post("/",recordingsController.createRecording);
+
+RecordingsRouter.get("/:id",recordingsController.getRecordingById);
+RecordingsRouter.put("/:id",recordingsController.updateRecordingById);
+ RecordingsRouter.delete("/:id",recordingsController.deleteRecordingById);
