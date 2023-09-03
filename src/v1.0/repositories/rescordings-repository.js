26dc4741e5a4c04366e5710230b3a1d0fea8e217
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 10:04:42 +0200

    recordings

--- src/v1.0/repositories/rescordings-repository.js
+++ src/v1.0/repositories/rescordings-repository.js
@@ -1,22 +1,22 @@
 const { prisma } = require("../config/db");
 
-const RecordingsRepository=()=>{
-    const createRecording=(data)=>{
-        return prisma.recordings.create({
-            data
-        })
-    }
-    const getRecordingById=async(id)=>{
-        return prisma.recordings.findUnique({
-            where:{
-                id:parseInt(id)
-            }
-        })
-    }
-    return {
-        createRecording,
-        getRecordingById,
-    }
-}
+const RecordingsRepository = () => {
+  const createRecording = (data) => {
+    return prisma.recordings.create({
+      data,
+    });
+  };
+  const getRecordingById = async (id) => {
+    return prisma.recordings.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+  };
+  return {
+    createRecording,
+    getRecordingById,
+  };
+};
 
-module.exports=RecordingsRepository();
+module.exports = RecordingsRepository();
