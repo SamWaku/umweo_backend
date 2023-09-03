81a4a6a73963a865559c037827319664b15dfcb2
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 11:38:36 +0200

    Models

--- src/v1.0/models/task-model.js
+++ src/v1.0/models/task-model.js
@@ -1,3 +1,13 @@
 const { prisma } = require("../config/db")
 
 exports.TaskModel=prisma.tasks
+
+// model Tasks {
+//     id          Int        @id @default(autoincrement())
+//     title       String
+//     description String
+//     therapist   Therapist? @relation(fields: [therapistId], references: [id])
+//     therapistId Int?
+//     user        User?      @relation(fields: [userId], references: [id])
+//     userId      Int?
+//   }
