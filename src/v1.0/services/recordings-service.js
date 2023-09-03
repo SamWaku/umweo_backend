b5ef8c8d68711f5f7193710577a2cf2f6295f04e
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 10:19:00 +0200

    recordings

--- src/v1.0/services/recordings-service.js
+++ src/v1.0/services/recordings-service.js
@@ -2,11 +2,16 @@ const recordingsRepository = require("../repositories/recordings-repository");
 
 const RecordingsService=()=>{
     const CreateRecording=async(data)=>{
-        const recording =recordingsRepository.createRecording(data);
+        const recording =await recordingsRepository.createRecording(data);
+        return recording;
+    }
+    const GetRecordingByID=async(id)=>{
+        const recording=await recordingsRepository.getRecordingById(id);
         return recording;
     }
     return{
-        CreateRecording
+        CreateRecording,
+        GetRecordingByID
     }
 }
 
