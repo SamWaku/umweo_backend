ea7b4070730c450ef323106706a42f9c822477ea
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-03 11:22:58 +0200

    recordings

--- src/v1.0/routers/recordings-router.js
+++ src/v1.0/routers/recordings-router.js
@@ -2,10 +2,18 @@ const recordingsController = require("../controllers/recordings-controller");
 
 const RecordingsRouter = require("express").Router();
 
+// create recording i.e /recordings/
 RecordingsRouter.post("/", recordingsController.createRecording);
 
+
+// get recording by id i.e /recordings/:id
 RecordingsRouter.get("/:id", recordingsController.getRecordingById);
+
+
+// update recording by id i.e /recordings/:id
 RecordingsRouter.put("/:id", recordingsController.updateRecordingById);
+
+// delete recording by id i.e /recordings/:id
 RecordingsRouter.delete("/:id", recordingsController.deleteRecordingById);
 
 module.exports=RecordingsRouter;
