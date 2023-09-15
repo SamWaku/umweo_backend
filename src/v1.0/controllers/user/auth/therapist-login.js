258cdf073e8d88546b3d1cc639bd906019edfd00
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-09-15 21:45:13 +0200

    changes

--- src/v1.0/controllers/user/auth/therapist-login.js
+++ src/v1.0/controllers/user/auth/therapist-login.js
@@ -1,4 +1,4 @@
-const{TherapistModel}=require('../../../models/therapist-model');
+const { TherapistModel } = require('../../../models/therapist-model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
@@ -10,13 +10,13 @@ const { JWT_SECRET } = process.env;
 // signup functionality
 exports.CreateTherapist = async (req, res) => {
     try {
-        const {name, email, password } = req.body;
+        const { name, email, password } = req.body;
         const userExists = await TherapistModel.findUnique({
             where: {
                 email: email
             }
         })
-        if(userExists){
+        if (userExists) {
             throw new Error("User already exists")
         }
 
@@ -29,10 +29,10 @@ exports.CreateTherapist = async (req, res) => {
         newLecture.password = bcrypt.hashSync(newUser.password, 12);
 
         await TherapistModel.create({
-            data:{
-                name:newLecture.name,
-                email:newLecture.email,
-                password:newLecture.password
+            data: {
+                name: newLecture.name,
+                email: newLecture.email,
+                password: newLecture.password
             }
         })
         res.status(201).json({
@@ -54,22 +54,22 @@ exports.LoginTherapist = async (req, res) => {
         const { email, password } = req.body;
         const user = await TherapistModel.findUnique({ where: { email: email } })
         if (!user) {
-            return res.status(401).json({ message: "Don't have an account? Please Register!",});
+            return res.status(401).json({ message: "Don't have an account? Please Register!", });
         }
-        
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch)return res.status(401).json({message: 'Incorrect password, try again'});
-            // await user.generateAuthToken()
-            //await user.generateRefreshToken();
-            const token = jwt.sign({"id":user._id, "email":user.email }, JWT_SECRET, {
-                expiresIn: '50m',
-            },)
 
-            res.status(200).send({
-                userid: user.id,
-                token: token,
-                message: "login successful",
-            });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ message: 'Incorrect password, try again' });
+        // await user.generateAuthToken()
+        //await user.generateRefreshToken();
+        const token = jwt.sign({ "id": user._id, "email": user.email }, JWT_SECRET, {
+            expiresIn: '50m',
+        },)
+
+        res.status(200).send({
+            userid: user.id,
+            token: token,
+            message: "login successful",
+        });
     } catch (error) {
         return res.status(500).json({
             success: false,
