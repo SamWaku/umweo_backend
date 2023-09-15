577528743afe46a7168dcea3ed3b8447f4fc994b
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-09-15 21:39:59 +0200

    changes

--- src/v1.0/controllers/user/auth/therapist-login.js
+++ src/v1.0/controllers/user/auth/therapist-login.js
@@ -1,1 +1,5 @@
 const{TherpistModel}=require('../../../models/therapist-model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
